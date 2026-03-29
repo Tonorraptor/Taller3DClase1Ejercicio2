767433448c3ecad832969c1ffc4c6df66f8594bb
Julio Celestino <velocirraptorrex3@gmail.com>
2026-03-29 17:45:27 -0500

    Add Proyect

---
+++ Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DClase1Ejercicio2
+{
+    internal class Rectangle
+    {
+        private float h;
+        private float b;
+
+        public Rectangle(float h, float b)
+        {
+            this.h = h;
+            this.b = b;
+        }
+        public virtual float GetArea()
+        {
+            return b * h;
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DClase1Ejercicio2
+{
+    internal class Triangle
+    {
+        private float h;
+        private float b;
+
+        public Triangle(float h, float b)
+        {
+            this.h = h;
+            this.b = b;
+        }
+        public virtual float GetArea()
+        {
+            return (b * h) / 2;
+        }
+    }
+}
